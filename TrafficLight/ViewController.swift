3bf1ba334830769322c815a6473f4940a00147b3
Manohin <111537419+Manohin@users.noreply.github.com>
2023-01-30 01:34:58 +0300

    Done!

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redColorView: UIView!
+    @IBOutlet var yellowColorView: UIView!
+    @IBOutlet var greenColorView: UIView!
+    @IBOutlet var button: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let views:[UIView] = [redColorView, yellowColorView, greenColorView]
+        
+     //   views.map({ $0.layer.cornerRadius = 60 }) - Почему-то возникает ошибка "Result of call to 'map' is unused"
+        
+                for view in views {
+                    view.layer.cornerRadius = 60
+                }
+        
+        button.layer.cornerRadius = 10
     }
-
-
+    
+    var isLighted = false
+    
+    func switchStatus(_ view: UIView) {
+        
+        if view.alpha != 1 {
+            view.alpha = 1
+            isLighted = true
+        } else {
+            view.alpha = 0.3
+            isLighted = false
+        }
+    }
+    
+    @IBAction func functionalButton() {
+        
+        if button.currentTitle == "START" {
+            switchStatus(redColorView)
+            button.setTitle("NEXT", for: .normal)
+        } else if redColorView.alpha == 1 && isLighted == true {
+            switchStatus(redColorView)
+            switchStatus(yellowColorView)
+        } else if yellowColorView.alpha == 1 && isLighted == true {
+            switchStatus(yellowColorView)
+            switchStatus(greenColorView)
+        } else if greenColorView.alpha == 1 && isLighted == true {
+            switchStatus(greenColorView)
+            switchStatus(redColorView)
+        }
+    }
+    
 }
 
